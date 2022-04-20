8b7f705dc022c74c11ae268ceaab34997a45dad0
Michał Starski <michal.starski@pm.me>
2022-04-20 23:18:02 +0200

    External spec download script

--- scripts/external-spec-dl.mjs
+++ scripts/external-spec-dl.mjs
@@ -1,3 +1,11 @@
 #!node_modules/.bin/zx
 
-$`echo Hello`;
+import { echo } from 'zx/experimental';
+
+echo(chalk.green('Downloading external spces 🚀'));
+
+const ROOT_DIR = path.resolve(__dirname, '../');
+
+await $`wget -O ${ROOT_DIR}/spec/lolbetpy.openapi.json http://lolbetpy:8000/openapi.json`;
+
+echo(chalk.green('Download complete.'));
